626a76f3071fe65493c59954cb3593a5650aa7bd
Simon Buchan <simon.buchan@skilitics.com>
2019-08-30 16:39:44 +1200

    Some small test file improvements.

--- test/index.ts
+++ test/index.ts
@@ -4,11 +4,24 @@ class TestError extends Error {
     name = 'TestError';
 }
 
+process.on('unhandledRejection', (error) => {
+    console.error("Unhandled rejection:\n%O", error);
+});
+
 process.on('uncaughtException', (error) => {
     console.error("Uncaught exception:\n%O", error);
 });
 
-catchErrors(() => {
+const notificationId = 125;
+const useWarningId = 456;
+const subitemNotificationId = 789;
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
+
+async function main() {
     const guid = "ded73175-c489-4f7e-acdc-3dbdde784468";
     const guid2 = "ded73175-c489-4f7e-acdc-3dbdde784469";
 
@@ -26,11 +39,11 @@ catchErrors(() => {
         { separator: true },
         { id: 103, text: "Time", disabled: true },
         { id: 124, text: "Counter" },
-        { id: 125, text: "Notification" },
+        { id: notificationId, text: "Notification" },
         {
             text: "Submenu", items: [
-                { id: 456, text: "Use warning" },
-                { id: 789, text: "Subitem Notification" },
+                { id: useWarningId, text: "Use warning" },
+                { id: subitemNotificationId, text: "Subitem Notification" },
             ]
         },
 
@@ -91,7 +104,7 @@ catchErrors(() => {
                     text: `Counter: ${++count}`,
                 });
                 return;
-            case 125:
+            case notificationId:
                 notifyIcon.update({
                     notification: {
                         icon: notificationIcon,
@@ -101,14 +114,8 @@ catchErrors(() => {
                     },
                 });
                 return;
-            case 456:
-                const useWarning = !item.checked;
-                contextMenu.update(itemId, {
-                    checked: useWarning,
-                });
-                notifyIcon.update({
-                    icon: useWarning ? altIcon : icon,
-                });
+            case useWarningId:
+                toggleIcon(item);
                 return;
         }
     });
@@ -123,25 +130,62 @@ catchErrors(() => {
 
     if (process.stdin.setRawMode)
         process.stdin.setRawMode(true);
-    process.stdout.write('Press any key to exit...\n');
-    process.stdin.on('data', catchErrors(() => {
-        notifyIcon.remove();
+    help();
+    for await (const chunk of process.stdin) {
+        switch (chunk.toString()) {
+            default:
+                help();
+                break;
+            case 'i':
+                toggleIcon(contextMenu.get(useWarningId));
+                break;
+            case 'q':
+                notifyIcon.remove();
+                process.exit();
+                break;
+        }
+    }
 
-        process.exit();
-    }));
-})();
+    function help() {
+        process.stdout.write('h: help | i: toggle icon | q: quit\n');
+    }
 
-// Catches and prints the error properties, including the `syscall` and `errno` for `Win32Error`s
-function catchErrors<Args extends any[], R, This = void>(body: (this: This, ...args: Args) => R): (this: This, ...args: Args) => (R | undefined) {
-    return function (...args) {
+    function toggleIcon(item: Menu.Item) {
+        const useWarning = !item.checked;
+        contextMenu.update(item.id, {
+            checked: useWarning,
+        });
+        notifyIcon.update({
+            icon: useWarning ? altIcon : icon,
+        });
+    }
+}
+
+function catchErrors<Args extends any[], R, This = void>(body: (this: This, ...args: Args) => R) {
+    return function (this: This, ...args: Args): R | void {
         try {
             return body.apply(this, args);
         } catch (e) {
-            console.error(e);
-            // This will not work if transpile target is less than es2015
-            if (e instanceof TestError) {
-                throw e;
-            }
+            handleError(e);
         }
     };
+}
+
+// Catches and prints the error properties, including the `syscall` and `errno` for `Win32Error`s
+function catchErrorsAsync<Args extends any[], R, This = void>(body: (this: This, ...args: Args) => Promise<R>) {
+    return async function (this: This, ...args: Args): Promise<R | void> {
+        try {
+            return await body.apply(this, args);
+        } catch (e) {
+            handleError(e);
+        }
+    };
+}
+
+function handleError(e: any) {
+    console.error(e);
+    // This will not work if transpile target is less than es2015
+    if (e instanceof TestError) {
+        throw e;
+    }
 }
